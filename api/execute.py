64336f0ef9ef692648002fb2e5896f98f9c00b3d
Rajarshi <impladimpla@gmail.com>
2025-07-05 21:22:38 +0530

    some more updates

--- api/execute.py
+++ api/execute.py
@@ -1,8 +1,7 @@
-import sys
-import io
-import contextlib
 from http.server import BaseHTTPRequestHandler
 import json
+import io
+import contextlib
 
 class handler(BaseHTTPRequestHandler):
     def do_POST(self):
@@ -11,12 +10,12 @@ class handler(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-Type', 'application/json')
             self.send_header('Access-Control-Allow-Origin', '*')
-            self.send_header('Access-Control-Allow-Methods', 'POST')
+            self.send_header('Access-Control-Allow-Methods', 'POST, OPTIONS')
             self.send_header('Access-Control-Allow-Headers', 'Content-Type')
             self.end_headers()
 
             # Get request body
-            content_length = int(self.headers['Content-Length'])
+            content_length = int(self.headers.get('Content-Length', 0))
             post_data = self.rfile.read(content_length)
             data = json.loads(post_data.decode('utf-8'))
             
@@ -69,6 +68,6 @@ class handler(BaseHTTPRequestHandler):
         # Handle CORS preflight
         self.send_response(200)
         self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Access-Control-Allow-Methods', 'POST')
+        self.send_header('Access-Control-Allow-Methods', 'POST, OPTIONS')
         self.send_header('Access-Control-Allow-Headers', 'Content-Type')
         self.end_headers() 
